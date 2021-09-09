b85ac638da51c0568fc1c5cb518664a0ba277258
Eric Anderson <ericjanderson4@gmail.com>
2021-09-09 11:24:09 -0600

    feat: Rate project

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 # Ao Cli
 
-Ao Cli is used to authenticate programmatic access to our app and delegate those permissions to things such as node/js scripts.
+`ao-cli` is used to authenticate programmatic access to our app and delegate those permissions to things such as node/js scripts.
 
 ## Setup
 
@@ -16,3 +16,40 @@ node -v
 - Get libraries from AO contact.  You should receive `ao-lib-v...` and `ao-cli-v...`
 
 - Install libraries
+```
+npm install --save anderson-optimization-ao-lib-v2-0-4.tgz
+npm install --save anderson-optimization-ao-cli-v2-0-4.tgz
+```
+
+- Test install
+```
+npm run help
+
+# response should be 
+> cli-setup@1.0.0 help /home/eric/work/cli-setup
+> ao help
+
+Usage: ao [options] [command]
+
+Options:
+  -V, --version   output the version number
+  -v, --verbose   verbose output
+  --jsonOutput    json output
+  -h, --help      display help for command
+
+Commands:
+  auth            Authentication to AO system
+  config          Configuration for ao
+  file            File system
+  resource        Resource system
+  task            Task system
+  ws              AO local workspace for deployment
+  help [command]  display help for command
+```
+
+## Examples
+
+```
+npm run user-info
+npm run fetch-projects
+```
---
+++ src/rate-asset-in-all-projects.js
@@ -0,0 +1,63 @@
+
+// ensure you run login script
+// npm run login
+
+
+const aoCli = require('@anderson-optimization/ao-cli');
+const _ = require('lodash');
+let aoLib;
+
+function *iterateAssetRefs(project){
+    for (let [stepKey,step] of Object.entries(project.step)){
+        if(!step.asset) continue;
+        for (let [groupKey,group] of Object.entries(step?.asset)){
+            for (let [assetKey,assetRef] of Object.entries(group)){
+                yield {
+                    stepKey,
+                    groupKey,
+                    assetKey,
+                    assetRef
+                }
+            }
+        }
+    }
+}
+
+async function main(){
+    try{
+        const [bin,script,assetRn, rating] = process.argv;
+        aoLib = await aoCli.getAoLib();
+        const userState = await aoLib.$user.getUserState();
+        const tid = userState.team; // this is the currently select team similar to the UI
+        console.log(`Searching all projects in current team=${tid} for assetRn=${assetRn}`)
+
+        const projects = await aoLib.resource.project.list({limitTo:1000,tid});
+
+        // Loop through each project and search for assetref
+        for (const [pid,project] of Object.entries(projects)){
+            const projectUpdate={
+                rn: project.rn
+            }
+            for (const {stepKey,groupKey,assetKey,assetRef} of iterateAssetRefs(project)){
+                const path=`step/${stepKey}/asset/${groupKey}/${assetKey}`;
+                if(assetRef.rn===assetRn){
+                    const currentRating = assetRef.rating;
+                    console.log(`Found asset in project ${project.rn} at ${path} currentRating=${currentRating}`);
+                    if(rating){
+                        projectUpdate[`${path}/rating`]=rating;
+                    }
+                }
+            }
+            if(_.size(projectUpdate)>1){
+                console.log('Updating project!!!',projectUpdate);
+                await aoLib.resource.project.update(projectUpdate);
+            }
+        }
+    } catch(err){
+        console.error(err);
+        console.error('Error running user info script');
+        process.exit(1)
+    }
+    process.exit(0);
+}
+main();
